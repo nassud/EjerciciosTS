e3157df813ad6de8db0070e8f7285c33736c29db
Sebastián Gómez Dussán <nassud@gmail.com>
2022-01-16 19:07:53 -0500

    Adicionados archivos a gitignore y enunciados para ejercicios 6 y 7

---
+++ ejercicios/Ejercicio6.ts
@@ -0,0 +1,2 @@
+// Escribe un programa que lea un número e imprime un triángulo de números de la forma
+// siguiente. Si el número leído es 4, imprimirá:
---
+++ ejercicios/Ejercicio7.ts
@@ -0,0 +1,3 @@
+// Dado un número natural N, se calcula la raíz digital de N sumando los dígitos que lo
+// componen. El proceso se repite sobre el nuevo número hasta que el resultado obtenido
+// tiene un sólo dígito. Este último número es la raíz digital del número N.
